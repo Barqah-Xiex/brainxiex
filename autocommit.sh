a43283694a34f29a92efedc916776d57f5daf9d7
Barqah-Xiex <barqahgaming@gmail.com>
2024-09-24 12:11:00 +0700

    Auto commit on 2024-09-24 12:11:00

--- autocommit.sh
+++ autocommit.sh
@@ -6,6 +6,9 @@ REPO_DIR="$(pwd)"
 # Masuk ke direktori repository
 cd "$REPO_DIR" || { echo "Direktori tidak ditemukan"; exit 1; }
 
+# Tarik perubahan dari remote repository
+git pull origin main --rebase # Ganti 'main' dengan 'master' jika perlu
+
 # Menambahkan semua perubahan
 git add .
 
