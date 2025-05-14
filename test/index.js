ecc5a46c4016f97d29e690bf9682054f55c6d07f
Barqah-Xiex <barqahgaming@gmail.com>
2025-05-14 08:08:54 +0700

    Auto commit on 2025-05-14 08:08:53

--- test/index.js
+++ test/index.js
@@ -1,6 +1,14 @@
 const brainxiex = require("../")({session_local: true});
 
-// brainxiex.ai("apakah rapunzel suka makan kerupuk?","brainxiex","brainxiex_module123")
-// .then(res => console.log(res.answer));
 
-console.log(Object.keys(brainxiex))
+brainxiex.api.ai({
+    model: 'brainxiex',
+    messages: [{ role: 'user', content: 'Apakah Rapunzel suka makan kerupuk ?' }],
+    sessionID: 'Hawimau'
+})
+.then(res => {
+    console.log(res);
+})
+.catch(err => {
+    console.error(err);
+});
