2c3ca018d135a5bb696ae9b6113642671aa512da
Barqah-Xiex <barqahgaming@gmail.com>
2025-05-14 08:30:48 +0700

    Auto commit on 2025-05-14 08:30:48

--- autocommit.sh
+++ autocommit.sh
@@ -6,9 +6,9 @@ REPO_DIR="$(pwd)"
 # Masuk ke direktori repository
 cd "$REPO_DIR" || { echo "Direktori tidak ditemukan"; exit 1; }
 
-npm version minor
+npm version minor;
 
-git remote -v
+git remote -v;
 
 # Tarik perubahan dari remote repository
 git pull origin main --rebase # Ganti 'main' dengan 'master' jika perlu
